a7890008d17124173ffe2772e4e9106b3fc934f5
vasu <vasun77@gmail.com>
2016-05-28 14:51:17 +0530

    added firebase login

--- android/src/main/java/in/nvasu/FirebaseBridge/FirebaseBridgeModule.java
+++ android/src/main/java/in/nvasu/FirebaseBridge/FirebaseBridgeModule.java
@@ -8,15 +8,29 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import com.facebook.AccessToken;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.Map;
 
 public class RNFirebaseModule extends ReactContextBaseJavaModule {
 
-  private static final String DURATION_SHORT_KEY = "SHORT";
-  private static final String DURATION_LONG_KEY = "LONG";
+  private FirebaseAuth mAuth;
+  private FirebaseAuth.AuthStateListener mAuthListener;
   
   public ToastModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    mAuth = FirebaseAuth.getInstance();
+    mAuthListener = new FirebaseAuth.AuthStateListener() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            Log.d("Firebase Login","onAuthStateChanged:signed_in:"+user.getUid());
+        } else {
+            Log.d("Firebase Login","onAuthStateChanged:signed_out:");
+        }
+    }
   }
 
   @Override
@@ -27,13 +41,41 @@ public class RNFirebaseModule extends ReactContextBaseJavaModule {
   @Override
   public Map<String, Object> getConstants() {
     final Map<String, Object> constants = new HashMap<>();
-    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
-    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
     return constants;
   }
 
+  @Override
+  public void onStart() {
+    super.onStart();
+    mAuth.addAuthStateListener(mAuthListener);
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    mAuth.addAuthStateListener(mAuthListener);
+  }
+
   @ReactMethod
-  public void show(String message, int duration) {
-    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  public void handleFacebookAccessToken(AccessToken token) {
+    Log.d("Firebase Login", "handleFacebookAccessToken:" + token);
+
+    AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+    mAuth.signInWithCredential(credential)
+      .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+          Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+          // If sign in fails, display a message to the user. If sign in succeeds
+          // the auth state listener will be notified and logic to handle the
+          // signed in user can be handled in the listener.
+          if (!task.isSuccessful()) {
+            Log.w(TAG, "signInWithCredential", task.getException());
+            Toast.makeText(FacebookLoginActivity.this, "Authentication failed.",
+            Toast.LENGTH_SHORT).show();
+          }
+        }
+    });
   }
 }
--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const FirebaseBridge = require("react-native").NativeModules.FirebaseBridge;
 
 class RNFirebase {
-
+  static handleFacebookAccessToken(token) {
+    console.log("javascript handleFacebookAccessToken", token);
+  }
 }
 
 module.exports = RNFirebase;
